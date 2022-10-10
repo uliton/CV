be480e5cdd4df5df406bd02f206c2a6280136bb8
Oleksandr Kharutsa <al.haruca@gmail.com>
2022-10-10 20:11:00 +0200

    add json file, routing, header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.scss';
+import { Route, Routes } from 'react-router';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { NotFound } from './components/NotFound';
+import { OtherProjects } from './components/OtherProjects';
 
-export const App: React.FC = () => {
-  return (
-    <div>
-      Hello!
-    </div>
-  );
-};
+export const App: React.FC = () => (
+  <>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<OtherProjects />} />
+      <Route path="/cv/" element={<OtherProjects />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+
+    <Footer /> 
+  </>
+);
